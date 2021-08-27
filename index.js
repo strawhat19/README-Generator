6172aa4cec33334c50b3357b7c727b635510a402
Rakib Ahmed <rakib987@gmail.com>
2021-08-27 14:20:55 -0400

    Added Node Packages

---
+++ index.js
@@ -0,0 +1,8 @@
+// README Generator
+console.log(`Professional README Generator for Busy Developers!`);
+
+// Declaring Constants
+const fs = require(`fs`);
+const inquirer = require(`inquirer`);
+
+
